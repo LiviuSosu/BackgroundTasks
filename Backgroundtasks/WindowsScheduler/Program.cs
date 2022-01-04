e266d640554a1a8c0aeb2cb7ea2289b377011f01
LiviuSosu <sosuliviu@gmail.com>
2022-01-04 17:37:21 +0100

    connected to sql server

--- Backgroundtasks/WindowsScheduler/Program.cs
+++ Backgroundtasks/WindowsScheduler/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Data;
+using System.Data.SqlClient;
+
+string connetionString = "Data Source = DESKTOP-M80MDUC\\SQLEXPRESS;Initial Catalog=BackgroundTasks;Integrated Security = True;";
+SqlConnection cnn;
+
+cnn = new SqlConnection(connetionString);
+cnn.Open();
+
+SqlCommand command;
+SqlDataAdapter adapter = new SqlDataAdapter();
+string sqlCommand = "DELETE FROM [dbo].[Articles] WHERE Id = 2;";
+
+command = new SqlCommand(sqlCommand, cnn);
+
+adapter.DeleteCommand = new SqlCommand(sqlCommand, cnn);
+adapter.DeleteCommand.ExecuteNonQuery();
+
+command.Dispose();
+cnn.Close();
+
 Console.WriteLine("Hello, World!");
